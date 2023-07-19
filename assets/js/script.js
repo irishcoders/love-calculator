5d2237a5cd3f3a5e3fc9325a3bfd351decceddba
irishcoders <>
2023-07-19 12:09:32 +0000

    Add addition and division question to  the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,52 @@
 /* Wait for the DOM to finish loading before running the game
  * Get the button elements and add event listeners to them
 */
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+    runGame("division");
+
 });
 
-function runGame() {
+/**
+ * The main game "loop" will be called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame(gameType) {
+    //This creates a random number between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivideQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknow game type: ${gameType}. Abborting!`;
+    }
 }
+/** 
+    //this is the division game code
+    
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknow game type: ${gameType}. Abborting!`;
+    }
+}
+*/
 
 function checkAnswer() {
 
@@ -39,18 +67,25 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion () {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
-function displaySubtractQuestion () {
+function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion () {
+function displayMultiplyQuestion() {
 
 }
 
-function displayDivideQuestion () {
+function displayDivideQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 
 }
